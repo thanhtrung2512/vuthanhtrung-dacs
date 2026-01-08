e311e3303c56ead0333211517ce9a26a91395bdb
thanhtrung2512 <23010378@st.phenikaa-uni.edu.vn>
2026-01-08 23:11:46 +0700

    Add files via upload

---
+++ spb3/spbn3/src/main/java/com/example/spbn3/Spbn3Application.java
@@ -0,0 +1,13 @@
+package com.example.spbn3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spbn3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spbn3Application.class, args);
+	}
+
+}
